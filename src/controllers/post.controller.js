09be9db6ed709b4660458d5181ebd9cdac752ac7
Chandra Prakash <p71203@gmail.com>
2026-02-28 23:15:13 +0530

    Post can be saved in Database

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/post.model")
 const ImageKit = require("@imagekit/nodejs")
 const { toFile } = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken")
 
 
 const imagekit = new ImageKit({
@@ -11,12 +12,42 @@ const imagekit = new ImageKit({
 async function createPostController(req,res){
     console.log(req.body, req.file)
 
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message: "Token not provided, Unauthorized access"
+        })
+    }
+
+    let decoded = null
+
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message: "user not authorized"
+        })
+    }
+
+    console.log(decoded)
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
-        fileName: "Test"
+        fileName: "Test",
+        folder: "cohort-2-insta-clone-posts"
     })
 
-    res.send(file)
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user:decoded.id
+    })
+
+    res.status(201).json({
+        message: "Post created successfully.",
+        post
+    })
 
 }
 
